c7dbbbc58e2ebfe071f14d6744a10de4909003d9
Uriel Carrera <a3520110324@alumno.uttehuacan.edu.mx>
2023-06-16 23:40:23 -0600

    Desarrollo de la interfaz -más

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,7 @@
 import { Component ,ViewChild } from '@angular/core';
 import { IonModal } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core/components';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -9,16 +10,18 @@ import { OverlayEventDetail } from '@ionic/core/components';
 export class Tab3Page {
   @ViewChild(IonModal) modal!: IonModal;
 
-  message = 'Sí deseás agregar una nueva medición haz click ';
+  message = '';
   name: string;
+
+  opcionSeleccionada: string;
+  opcionSeleccionada1: string;
+  fechaHoraActual: string;
 
   cancel() {
     this.modal.dismiss(null, 'cancel');
   }
 
-  confirm() {
-    this.modal.dismiss(this.name, 'confirm');
-  }
+  
 
   onWillDismiss(event: Event) {
     const ev = event as CustomEvent<OverlayEventDetail<string>>;
@@ -27,11 +30,32 @@ export class Tab3Page {
     }
   }
 
-  constructor() {
+  constructor(private router: Router) {
 
     this.modal = this.modal;
-    this.name="Usuario"
+    this.name=""
+    this.opcionSeleccionada = '';
+    this.opcionSeleccionada1 = '';
+    this.fechaHoraActual=""
+    this.obtenerFechaHoraActual();
     
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+    this.router.navigate(['/tabs', 'tab1']);
+    
+  }
+
+  obtenerFechaHoraActual() {
+    const fechaHora = new Date();
+    const dia = fechaHora.getDate();
+    const mes = fechaHora.getMonth() + 1; // Los meses se indexan desde 0
+    const año = fechaHora.getFullYear();
+    const hora = fechaHora.getHours();
+    const minutos = fechaHora.getMinutes();
+
+    this.fechaHoraActual = `${dia}/${mes}/${año} ${hora}:${minutos}`;
   }
 
 }
