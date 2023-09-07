64c0c9d6ee7fcb1a88c7cebc3822c25df861f61f
Uriel Carrera <a3520110324@alumno.uttehuacan.edu.mx>
2023-09-07 12:30:23 -0600

    Versión 1 -complete

--- src/app/Service/service.service.ts
+++ src/app/Service/service.service.ts
@@ -4,52 +4,54 @@ import { getDatabase, ref, set, child, get, push } from 'firebase/database';
 @Injectable({
   providedIn: 'root',
 })
-export class ServiceService {
-  countBeat: any = 0;
-  medir: any = 0;
+
+//docmuentar cada función y cada variable
+export class ServiceService { //clase para el servicio
+  countBeat: any = 0; //variable para contar los pulsos
+  medir: any = 0; //variable para medir el pulso
   constructor() {
-    console.log('hola');
+    //constructor de la clase
+    console.log('Inicio');
   }
 
   async leerDatos() {
+    //función para leer los datos de la base de datos
     const database = getDatabase(app);
     const dbRef = ref(database);
 
-
+    // Obtener los datos de la base de datos
     return await get(child(dbRef, `/pulso`)).then((snapshot) => {
-      console.log(snapshot.val());
-      return snapshot.val();
+      console.log(snapshot.val()); //
+      return snapshot.val(); //retorna el valor de la base de datos
     });
   }
 
-
   async getRegistros() {
+    //función para obtener los registros de la base de datos
     const database = getDatabase(app);
     const dbRef = ref(database);
 
     return await get(child(dbRef, `/registros`)).then((snapshot) => {
-
-      
       console.log(snapshot.val());
       return snapshot.val();
     });
   }
+  //función para escribir los datos en la base de datos
   async escribirDatos(registro: any) {
     const database = getDatabase(app);
     const dbRef = ref(database, 'registros/');
 
-      let result =  await push(dbRef, registro);
-   
-      // Manejar el caso cuando 'registros.pulsos' no está definida o es 'undefined'
-      console.log(result);
-     
+    let result = await push(dbRef, registro);
+
+    // Manejar el caso cuando 'registros.pulsos' no está definida o es 'undefined'
+    console.log(result);
   }
-  async medirPulso(estado:boolean) {
+  //función para medir el pulso 0 desactivr medición
+  async medirPulso(estado: boolean) {
     const database = getDatabase(app);
     const dbRef = ref(database, 'medir/');
-    await  set(dbRef, {
-      medir: estado, 
+    await set(dbRef, {
+      medir: estado,
     });
   }
-
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,116 +1,88 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { PipeTransform } from '@angular/core';
-import { Chart, registerables} from 'chart.js';
+import { Chart, registerables } from 'chart.js';
 import { ServiceService } from '../Service/service.service';
-
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page implements OnInit , PipeTransform  {
-  
-
-  @ViewChild('heartRateCanvas', { static: true }) heartRateCanvas! : ElementRef;
+export class Tab1Page implements OnInit, PipeTransform {
+  @ViewChild('heartRateCanvas', { static: true }) heartRateCanvas!: ElementRef;
   heartRateChart!: Chart;
 
-
-  @ViewChild('temperatureCanvas', { static: true }) temperatureCanvas!: ElementRef;
+  @ViewChild('temperatureCanvas', { static: true })
+  temperatureCanvas!: ElementRef;
   temperatureChart!: Chart;
 
+  registro = {
+    id: 1,
+    pulsos: 80,
+    resultado: 'Normal',
+    descripcion: 'Ritmo cardíaco regular',
+    fecha_medicion: '2023-08-02T10:30:00',
+    nombre_paciente: 'Jose Pérez',
+  };
+  registros: any;
+  transformRegistros: any = [];
+  beatCount = 0;
 
-
-
-    registro = {
-      "id": 1,
-      "pulsos": 80,
-      "resultado": "Normal",
-      "descripcion": "Ritmo cardíaco regular",
-      "fecha_medicion": "2023-08-02T10:30:00",
-      "nombre_paciente": "Jose Pérez"
-    }
-    registros : any ;
-    transformRegistros: any = [];
-    beatCount = 0;
-
-
-  constructor( private service:ServiceService) {
-    console.log("hola");
+  constructor(private service: ServiceService) {
+    console.log('hola');
     this.registros = this.service.getRegistros();
-    this.transformRegistros =  this.transform(this.registros);
-    
+    this.transformRegistros = this.transform(this.registros);
 
-    this.beatCount = this.transformRegistros[this.transformRegistros.length - 1].pulsos ;
- 
-    
-    
-    
+    this.beatCount =
+      this.transformRegistros[this.transformRegistros.length - 1].pulsos;
   }
   transform(value: any): any[] {
     // Convierte el objeto de registros en un arreglo y devuelve el resultado
-   
-    
- 
-      return Object.keys(value).map((key) => value[key]);
-  
+
+    return Object.keys(value).map((key) => value[key]);
   }
 
-
-
   async ngOnInit() {
-    
-  
-    
     this.registros = await this.service.getRegistros();
-    this.transformRegistros =  this.transform(this.registros);
-    this.beatCount =  this.transformRegistros[this.transformRegistros.length - 1].pulsos;
+    this.transformRegistros = this.transform(this.registros);
+    this.beatCount =
+      this.transformRegistros[this.transformRegistros.length - 1].pulsos;
     Chart.register(...registerables);
 
     this.createHeartRateChart();
-
-     
-    
   }
-  async  ionViewWillEnter(){
-
+  async ionViewWillEnter() {
     this.registros = await this.service.getRegistros();
-    this.transformRegistros =  this.transform(this.registros);
-    this.beatCount =  this.transformRegistros[this.transformRegistros.length - 1].pulsos;
+    this.transformRegistros = this.transform(this.registros);
+    this.beatCount =
+      this.transformRegistros[this.transformRegistros.length - 1].pulsos;
     console.log(this.transformRegistros);
     this.createHeartRateChart();
   }
-
 
   createHeartRateChart() {
     const heartRateCanvasElement = this.heartRateCanvas.nativeElement;
     this.heartRateChart = new Chart(heartRateCanvasElement, {
       type: 'line',
       data: {
-        labels: ['0S','60S'],
-        datasets: [{
-          label: '',
-          data: [0,this.beatCount],
-          borderColor: 'red',
-          backgroundColor: 'transparent'
-        }]
+        labels: ['0S', '60S'],
+        datasets: [
+          {
+            label: '',
+            data: [0, this.beatCount],
+            borderColor: 'red',
+            backgroundColor: 'transparent',
+          },
+        ],
       },
       options: {
         responsive: true,
         scales: {
           y: {
-            beginAtZero: true
-          }
-        }
-      }
+            beginAtZero: true,
+          },
+        },
+      },
     });
   }
-
-  
-
- 
-
-
-
 }
-
